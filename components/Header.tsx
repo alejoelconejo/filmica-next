d28434cdf2ed4d0eb4c8cbf66dc3b601bde08fc6
Alejo Janin <alejojanin@gmail.com>
2022-12-23 18:17:41 -0300

    Add nav to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,28 @@
+import Link from 'next/link'
+
 export const Header = () => {
   return (
-    <header className='max-w-5xl mx-auto py-4'>
-      <h1 className='text-5xl font-bold'>Filmica</h1>
-      <h2 className='text-orange-500 text-3xl font-semibold'>
-        Discover your favourite films
-      </h2>
+    <header className='max-w-5xl mx-auto py-4 flex justify-between'>
+      <div>
+        <h1 className='text-5xl font-bold'>Filmica</h1>
+        <p className='text-orange-500 text-3xl font-semibold'>
+          Discover your favourite films
+        </p>
+      </div>
+      <nav className='flex items-center'>
+        <ul className='flex gap-4 items-center'>
+          <li>
+            <Link href='/' className='text-xl font-semibold'>
+              <h3>Home</h3>
+            </Link>
+          </li>
+          <li>
+            <Link href='/discover' className='text-xl font-semibold'>
+              <h3>Discover</h3>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
