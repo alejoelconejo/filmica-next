41976bd063c9c460247578d2a37b2857417758c6
Alejo Janin <alejojanin@gmail.com>
2022-12-23 12:08:53 -0300

    Fix header style

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <header className='max-w-5xl mx-auto mb-4'>
+    <header className='max-w-5xl mx-auto py-4'>
       <h1 className='text-5xl font-bold'>Filmica</h1>
       <h2 className='text-orange-500 text-3xl font-semibold'>
         Discover your favourite films
