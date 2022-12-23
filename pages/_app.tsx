372374bef88224b8a06943467e69d6ce5f4d3946
Alejo Janin <alejojanin@gmail.com>
2022-12-23 12:09:20 -0300

    Add Inter font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { FavoritesProvider } from '../contexts/FavoriteContext'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${inter.variable} font-sans`}>
+      <FavoritesProvider>
+        <Component {...pageProps} />
+      </FavoritesProvider>
+    </div>
+  )
 }
