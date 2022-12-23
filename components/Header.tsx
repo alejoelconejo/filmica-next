3f6d3ae9e94c9870d202f8c7890c474098a06374
Alejo Janin <alejojanin@gmail.com>
2022-12-23 10:51:09 -0300

    Add header

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <header className='max-w-5xl mx-auto mb-4'>
+      <h1 className='text-5xl font-bold'>Filmica</h1>
+      <h2 className='text-orange-500 text-3xl font-semibold'>
+        Discover your favourite films
+      </h2>
+    </header>
+  )
+}
