938a740c899c5a117a0fbc546680e3b125f0e06c
Alejo Janin <alejojanin@gmail.com>
2022-12-24 19:55:16 -0300

    Remove unused font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { FavoritesProvider } from '../contexts/FavoriteContext'
-import { Headland_One, Inter } from '@next/font/google'
+import { Inter } from '@next/font/google'
 import Head from 'next/head'
 import { Layout } from '../components/Layout'
 
