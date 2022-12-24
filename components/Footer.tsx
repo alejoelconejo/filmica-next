05c963089cad3499b0e08a4e0519ec32e0fa7d87
Alejo Janin <alejojanin@gmail.com>
2022-12-24 19:54:56 -0300

    Add style to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className='max-w-5xl mx-auto py-4'>
+    <footer className='max-w-5xl mx-auto py-4 px-2 md:px-0'>
       Developed by alejoelconejo 🐰
     </footer>
   )
