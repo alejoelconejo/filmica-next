12a4b838e58feee0e017f48c6b85c96c697e89f7
Alejo Janin <alejojanin@gmail.com>
2022-12-24 19:55:29 -0300

    Fix style

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ export const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <Header />
-      <main className='max-w-5xl mx-auto'>{children}</main>
+      <main className='max-w-5xl mx-auto px-2 md:px-0'>{children}</main>
       <Footer />
     </>
   )
