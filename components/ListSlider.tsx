71387c3bcc295b2bcae0e078ec5b0675a6aa8441
Alejo Janin <alejojanin@gmail.com>
2022-12-26 09:21:34 -0300

    Add list slider component

---
+++ components/ListSlider.tsx
@@ -0,0 +1,22 @@
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
+
+export const ListSlider = ({ children }: any) => {
+  const [sliderRef] = useKeenSlider({
+    breakpoints: {
+      '(max-width: 600px)': {
+        slides: { perView: 3, spacing: 10 },
+      },
+    },
+    slides: {
+      perView: 5,
+      spacing: 10,
+    },
+  })
+
+  return (
+    <ul className='keen-slider' ref={sliderRef}>
+      {children}
+    </ul>
+  )
+}
