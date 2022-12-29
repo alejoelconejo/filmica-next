22ac3eae374cbf7d24cc3164a82bb7274f559548
Alejo Janin <alejojanin@gmail.com>
2022-12-29 11:40:08 -0300

    Create api getMovies function

--- api.ts
+++ api.ts
@@ -1,5 +1,14 @@
+import { MovieList, MovieListResult } from './types'
+
 export const API_KEY_PUBLIC = process.env.NEXT_PUBLIC_API_KEY
 export const API_KEY = process.env.API_KEY
 export const API_BASE_URL = 'https://api.themoviedb.org/3'
 export const API_IMG_URL = 'https://image.tmdb.org/t/p/w342'
 export const API_DEFAULT_LANGUAGE = 'en-US'
+
+export const getMovies = async (endpoint: string) => {
+  const res = await fetch(endpoint)
+  const data: MovieList = await res.json()
+  const movies: MovieListResult[] = data.results.slice(0, 10)
+  return movies
+}
