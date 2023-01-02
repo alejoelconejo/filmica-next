a763d31b06f47e5a18a08150e561b72d5256607c
Alejo Janin <alejojanin@gmail.com>
2023-01-02 08:41:27 -0300

    Add login button placeholder

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -7,7 +7,7 @@ export default function LoginButton() {
 
   if (session) {
     return (
-      <div className='flex gap-4'>
+      <div className='flex gap-2'>
         <button onClick={() => signOut()}>Sign Out</button>
         {session.user?.image && (
           <Link href='/profile'>
@@ -25,8 +25,21 @@ export default function LoginButton() {
     )
   }
   return (
-    <>
+    <div className='flex gap-2'>
       <button onClick={() => signIn()}>Sign In</button>
-    </>
+      <svg
+        className='w-9 h-9 text-gray-500'
+        aria-hidden='true'
+        fill='currentColor'
+        viewBox='0 0 20 20'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path
+          fill-rule='evenodd'
+          d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z'
+          clip-rule='evenodd'
+        ></path>
+      </svg>
+    </div>
   )
 }
