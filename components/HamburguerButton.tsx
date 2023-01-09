ada3d6c27d11177571e73b429ece1eea0ba697a3
Alejo Janin <alejojanin@gmail.com>
2023-01-09 12:14:09 -0300

    Add aria label to hamburguer button

--- components/HamburguerButton.tsx
+++ components/HamburguerButton.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 export const HamburgerButton = ({ isMenuOpen, toggleMenu }: Props) => {
   return (
-    <button className='relative group' onClick={toggleMenu}>
+    <button className='relative group' onClick={toggleMenu} aria-label='Menu'>
       <div className='relative flex overflow-hidden items-center justify-center rounded-full w-[40px] h-[40px] transform transition-all bg-slate-700 ring-0 ring-gray-300 hover:ring-8 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md'>
         <div className='flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden'>
           <div
