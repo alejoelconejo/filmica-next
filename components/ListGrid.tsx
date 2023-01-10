9dab311cd51e8f4d7de53bc89de12c6c94f21d55
Alejo Janin <alejojanin@gmail.com>
2023-01-10 20:11:51 -0300

    Fix grid items width

--- components/ListGrid.tsx
+++ components/ListGrid.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react'
 export const ListGrid = ({ children }: { children: ReactNode }) => {
   return (
     <div className='min-h-screen'>
-      <ul className='grid grid-cols-[repeat(auto-fill,_minmax(70px,_1fr))] md:grid-cols-[repeat(auto-fill,_minmax(125px,_1fr))] gap-2 md:gap-4'>
+      <ul className='grid grid-cols-[repeat(auto-fill,_minmax(100px,_1fr))] md:grid-cols-[repeat(auto-fill,_minmax(125px,_1fr))] gap-2 md:gap-4'>
         {children}
       </ul>
     </div>
